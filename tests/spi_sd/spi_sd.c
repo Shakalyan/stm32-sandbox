a8245fcb4a729bce1aed94a8a3b0b1170cd2ac9c
Shakalyan <vdovitg@gmail.com>
2025-11-20 21:22:10 +0300

    sd in progress...

--- tests/spi_sd/spi_sd.c
+++ tests/spi_sd/spi_sd.c
@@ -19,63 +19,98 @@ static void sd_to_idle(void)
     __DSB();
     __ISB();
     mdelay(TIMER, 10);
-    for (i = 0; i < 80; ++i) {
+    for (i = 0; i < 50; ++i) {
         spi_txrx(SPI, 0xFF);
     }
 }
 
-// static uint8_t sd_send_cmd(uint8_t cmd, uint32_t arg)
+
+// static void sd_send_cmd8(void)
 // {
-//     return 0;
+//     // send CMD0
+//     gpio_unset_pin(GPIO, 6);
+//     __DSB();
+//     __ISB();
+//     mdelay(TIMER, 10);
+//     spi_txrx(SPI, 0xFF);
+//     spi_txrx(SPI, 0xFF);
+//     spi_txrx(SPI, 0xFF);
+
+//     spi_txrx(SPI, 0x48);
+//     spi_txrx(SPI, 0x00);
+//     spi_txrx(SPI, 0x00);
+//     spi_txrx(SPI, 0x01);
+//     spi_txrx(SPI, 0xAA);
+//     spi_txrx(SPI, 0x87); // CRC
 // }
 
-static void sd_send_cmd0(void)
+
+void clear_print_read_buffer()
 {
-    spi_txb_await_empty(SPI);
-    spi_rxb_clear(SPI);
-
-    // send CMD0
-    gpio_unset_pin(GPIO, 6);
-    __DSB();
-    __ISB();
-    mdelay(TIMER, 10);
-    spi_txrx(SPI, 0xFF);
-    spi_txrx(SPI, 0xFF);
-    spi_txrx(SPI, 0xFF);
-
-    spi_txrx(SPI, 0x40);
-    spi_txrx(SPI, 0x00);
-    spi_txrx(SPI, 0x00);
-    spi_txrx(SPI, 0x00);
-    spi_txrx(SPI, 0x00);
-    spi_txrx(SPI, 0x95); // CRC
-
-    pr_info("cmd0 ans:\n");
-    for (int i = 0; i < 8; ++i) {
-        uint8_t r = spi_txrx(SPI, 0xFF);
-        pr_info("i:%d, r1 = %x\n", i, (uint32_t)r);
+    uint8_t r;
+    while ((SPI->SR & (1<<0))) {
+        pr_info("rbd: %x\n", SPI->DR);
     }
 }
 
 
-static void sd_send_cmd8(void)
+static void sd_cs_down()
 {
-    // send CMD0
     gpio_unset_pin(GPIO, 6);
     __DSB();
     __ISB();
     mdelay(TIMER, 10);
+}
+
+static void sd_cs_up()
+{
+    gpio_set_pin(GPIO, 6);
+    __DSB();
+    __ISB();
+    mdelay(TIMER, 10);
+    spi_txrx(SPI, 0xFF);
+}
+
+
+static int sd_send_cmd(uint8_t cmd, uint32_t arg, uint8_t crc, uint32_t *r)
+{
+    int i = 0;
+    uint8_t byte;
+
+    spi_txb_await_empty(SPI);
+    spi_rxb_clear(SPI);
+    sd_cs_down();
+
     spi_txrx(SPI, 0xFF);
     spi_txrx(SPI, 0xFF);
     spi_txrx(SPI, 0xFF);
 
-    spi_txrx(SPI, 0x48);
-    spi_txrx(SPI, 0x00);
-    spi_txrx(SPI, 0x00);
-    spi_txrx(SPI, 0x01);
-    spi_txrx(SPI, 0xAA);
-    spi_txrx(SPI, 0x87); // CRC
+    spi_txrx(SPI, (cmd | 0x40) & 0x7F);
+
+    for (i = 0; i < 4; ++i) {
+        byte = (arg>>(8*(3-i))) & 0xFF;
+        spi_txrx(SPI, byte);
+    }
+
+    spi_txrx(SPI, crc | 1);
+
+    //clear_print_read_buffer();
+    for (i = 0; i < 8; ++i) {
+        *r = spi_txrx(SPI, 0xFF);
+        //pr_info("r = %x\n", *r);
+        if (*r != 0xFF) {
+            pr_info("r =  %x\n", *r);
+            return 0;
+        }
+    }
+
+    //sd_cs_up();
+
+    return 0;
 }
+
+
+
 
 
 void main(void)
@@ -93,11 +128,25 @@ void main(void)
     sd_to_idle();
 
 
-    int tries = 5;
-    while (tries--) {
-        sd_send_cmd0();
+    int tries = 100, i;
+    uint32_t r = 0;
+    while ((r != 1) && (tries--)) {
+        sd_send_cmd(0, 0, 0x95, &r);
     }
 
+    if (r != 1) {
+        pr_info("cmd0 failed\n");
+        pr_info("tries left: %d\n", tries);
+        goto Finish;
+    }
 
+    // sd_send_cmd(8, 0x1AA, 0x87);
+    // for (i = 0; i < 16; ++i) {
+    //     r = spi_txrx(SPI, 0xFF);
+    //     pr_info("cmd8 r: %x\n", r);
+    // }
+
+
+Finish:
     while (1);
 }
